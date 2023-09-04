1728e0b0fcc888255fb25a312c3f48b63f7debf3
BielovaIryna <130380331+BielovaIryna@users.noreply.github.com>
2023-09-04 19:58:45 +0000

    Deploying to gh-pages from @ BielovaIryna/goit-js-hw-09@d12c6c857503f3fce9c1b70f3f622aaf83d00359 🚀

---
+++ 03-promises.e93fab56.js
@@ -0,0 +1,2 @@
+document.querySelector("input[name=delay]"),document.querySelector("input[name=step]"),document.querySelector("input[name=amount]");document.querySelector("button").addEventListener("click",(()=>{for(let e=0;e<3;e+=1)setTimeout((()=>{shouldResolve(e,1e3+500*e)}),1e3+500*e)}));
+//# sourceMappingURL=03-promises.e93fab56.js.map
